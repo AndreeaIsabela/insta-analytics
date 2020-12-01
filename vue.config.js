7533ca586771ede94c3c0145853138fa2a9fb2bd
AndreeaIsabela <bala.andreea@yahoo.com>
2020-12-01 16:30:02 +0200

    [main] Add vue config file.

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  lintOnSave: false
+  chainWebpack: config => {
+    config.module
+      .rule('pug')
+      .test(/\.pug$/)
+      .use('pug-plain-loader')
+      .loader('pug-plain-loader')
+      .end();
+  }
 }
