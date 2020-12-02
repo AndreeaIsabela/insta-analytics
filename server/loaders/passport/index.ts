f4e835d7e4f56a4cc5ad8a35161e7c5f0054c433
Florian.B <floryn93@gmail.com>
2020-12-02 23:36:25 +0200

    [backend] Serialize and deserialze the user object for passport.

--- server/loaders/passport/index.ts
+++ server/loaders/passport/index.ts
@@ -1,5 +1,19 @@
 import { instagramStrategy } from './strategies/instagram';
 
 export async function passportLoader(passport): Promise<void> {
+  /**
+   * Used to serialize the user object.
+   */
+  passport.serializeUser((user, done) => {
+    done(null, user);
+  });
+
+  /**
+   * Used to deserialize the user objet.
+   */
+  passport.deserializeUser(async (user, done) => {
+    done(null, user);
+  });
+
   instagramStrategy(passport);
 };
