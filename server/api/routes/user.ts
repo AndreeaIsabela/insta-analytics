831b89470155227d4b4f521e1fdd39d09078d011
Florian.B <floryn93@gmail.com>
2020-12-02 11:51:12 +0200

    [backend] Add user routes.

---
+++ server/api/routes/user.ts
@@ -0,0 +1,21 @@
+import { authenticate } from 'passport';
+import { Request, Response, Router } from 'express';
+
+export function userRouter(): Router {
+  const router: Router = Router();
+
+  /**
+   * Route used to authenticate instagram user.
+   */
+  router.get('/user/auth/instagram', authenticate('instagram'));
+
+  /**
+   * Callback route used for instagram authentication.
+   */
+  router.post('/user/auth/instagram/redirect',
+    authenticate('instagram', { failureRedirect: '/' }),
+    (req: Request, res: Response): void => res.redirect('/photos')
+  );
+
+  return router;
+}
