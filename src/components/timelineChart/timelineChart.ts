9f2caed43b1b34379cd418d4b66671d89ebfa53b
AndreeaIsabela <bala.andreea@yahoo.com>
2020-12-02 03:46:34 +0200

    [timeline-chart] Add timeline chart logic.

--- src/components/timelineChart/timelineChart.ts
+++ src/components/timelineChart/timelineChart.ts
@@ -1,12 +1,136 @@
-import { defineComponent, ref } from 'vue'
+import { defineComponent, reactive, toRefs, watch, nextTick, onBeforeUnmount, onMounted } from 'vue'
+// import { Component, Watch, Prop, Vue } from 'vue-property-decorator'
+import { create, color, Scrollbar, Rectangle, options, Circle } from '@amcharts/amcharts4/core'
+import { Bullet, XYCursor, XYChart, Legend, DateAxis, ValueAxis, LineSeries } from '@amcharts/amcharts4/charts'
 
 const TimelineChart = defineComponent({
-  setup () {
-    const ceva = ref('altceva')
+  props: {
+    categories!: {
+      type: Array,
+      default: () => {
+        return []
+      }
+    },
+    data!: {
+      type: Array,
+      default: () => {
+        return []
+      }
+    },
+    name!: {
+      type: String,
+      default: 'chartdiv'
+    }
+  },
+  setup (props) {
+    let chart: any | null = reactive({})
+    const colorList: string[] = [
+      '#24262B',
+      '#ED008C',
+      '#00FCD4',
+      '#FF7E4C',
+      '#850082'
+    ]
+    const { categories, data, name } = toRefs(props)
 
+    const setSeriesBullets = function (series: any, bulletType: string): void {
+      let bullet
+
+      if (bulletType === 'circle') {
+        const white = '#fff'
+        bullet = series.bullets.push(new Circle())
+        bullet.strokeWidth = 2
+        bullet.radius = 5
+
+        bullet.fill = color(white)
+      } else if (bulletType === 'rectangle') {
+        bullet = series.bullets.push(new Bullet())
+        const rectangle = bullet.createChild(Rectangle)
+        bullet.horizontalCenter = 'middle'
+        bullet.verticalCenter = 'middle'
+        bullet.width = 8
+        bullet.height = 8
+        rectangle.width = 8
+        rectangle.height = 8
+      }
+
+      const bullethover = bullet.states.create('hover')
+      bullethover.properties.scale = 1.3
+    }
+
+    const createSeries = function (category: any, seriesColor: string): void {
+      options.queue = true
+      options.minPolylineStep = 5
+      options.onlyShowOnViewport = true
+      const series = chart.series.push(new LineSeries())
+      series.dataFields.valueY = category
+      series.dataFields.dateX = 'date'
+      series.name = category
+      series.stroke = color(seriesColor)
+      series.strokeWidth = 3
+      series.stacked = false
+      series.minBulletDistance = 20
+      series.tooltip.fontSize = '12px'
+      series.tooltipText = '{name.value}: [bold]{valueY}[/]'
+      series.fill = color(seriesColor)
+
+      const bulletType = 'circle'
+
+      setSeriesBullets(series, bulletType)
+      chart.invalidateData()
+    }
+
+    const setChartCursor = function (): void {
+      const red = '#E70036'
+      chart.cursor = new XYCursor()
+      chart.cursor.lineX.stroke = color(red)
+      chart.cursor.lineX.strokeDasharray = ''
+      chart.cursor.lineY.disabled = true
+    }
+
+    const initLineChart = async function (): Promise<void> {
+      console.log(name)
+      chart = create(`chart-${name.value}`, XYChart)
+      chart.responsive.enabled = true
+      // Set input format for the dates
+      chart.dateFormatter.inputDateFormat = 'yyyy-MM-dd'
+      // Create axes
+      const dateAxis = chart.xAxes.push(new DateAxis())
+      dateAxis.renderer.minGridDistance = 50
+      const valueAxis = chart.yAxes.push(new ValueAxis())
+      valueAxis.cursorTooltipEnabled = false
+      // Add data
+      chart.data = data.value
+
+      const categoriesLength: number = categories.value.length
+
+      for (let i = 0; i < categoriesLength; i++) {
+        createSeries(categories.value[i], colorList[i])
+      }
+
+      setChartCursor()
+
+      chart.legend = new Legend()
+      chart.scrollbarX = new Scrollbar()
+    }
+
+    watch(data, async (): Promise<void> => {
+      await nextTick()
+      initLineChart()
+    })
+
+    onMounted(async (): Promise<void> => {
+      await initLineChart()
+    })
+
+    onBeforeUnmount((): void => {
+      if (chart) {
+        chart.dispose()
+      }
+    })
     // expose to template
     return {
-      ceva
+      chartName: name.value
     }
   }
 })
