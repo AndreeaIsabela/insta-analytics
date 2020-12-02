229931493e055c92767f14a5004a16b5569ddafc
AndreeaIsabela <bala.andreea@yahoo.com>
2020-12-03 01:49:59 +0200

    [pie-chart] Add pie-chart logic.

--- src/components/pieChart/pieChart.ts
+++ src/components/pieChart/pieChart.ts
@@ -1,14 +1,124 @@
-import { defineComponent, ref } from 'vue'
+import { defineComponent, reactive, watch, nextTick, toRefs, onBeforeUnmount } from 'vue'
+import { create, color, ColorSet, MouseCursorStyle } from '@amcharts/amcharts4/core'
+import { PieChart, PieSeries, Legend } from '@amcharts/amcharts4/charts'
 
-const PieChart = defineComponent({
-  setup () {
-    const ceva = ref('altceva')
+const PieChartComponent = defineComponent({
+  props: {
+    categories: {
+      type: Array,
+      default: () => {
+        return []
+      }
+    },
+    data: {
+      type: Array,
+      default: () => {
+        return []
+      }
+    },
+    name: {
+      type: String,
+      default: 'chartdiv'
+    },
+    value: {
+      type: Number,
+      default: 0
+    },
+    category: {
+      type: String,
+      default: ''
+    }
+  },
+  setup (props) {
+    let pieSeries: any | null = reactive({})
+    let chart: any | null = reactive({})
+    const colorList: string[] = [
+      '#B169DC',
+      '#ED008C',
+      '#9ED228',
+      '#FF7E4C',
+      '#D14972'
+    ]
+    const { categories, data, name, value, category } = toRefs(props)
+
+    const createSeries = (): void => {
+      const white = '#FFFFFF'
+      pieSeries = chart.series.push(new PieSeries())
+      pieSeries.dataFields.value = value.value
+      pieSeries.dataFields.category = category.value || categories.value[0]
+      pieSeries.slices.template.stroke = color(white)
+      pieSeries.slices.template.strokeWidth = 2
+      pieSeries.slices.template.strokeOpacity = 1
+
+      pieSeries.labels.template.maxWidth = 350
+      pieSeries.labels.template.truncate = true
+
+      pieSeries.slices.template.cursorOverStyle = MouseCursorStyle.pointer
+    }
+
+    const createSeriesAnimations = (): void => {
+      pieSeries.hiddenState.properties.endAngle = -90
+      pieSeries.hiddenState.properties.opacity = 1
+      pieSeries.labels.template.text = '{category.value}: {value.value}'
+      pieSeries.labels.template.fontSize = '14px'
+      const screenWidth = window.innerWidth
+      if (screenWidth < 992) {
+        pieSeries.labels.template.text = ''
+      }
+    }
+
+    const setColors = (): void => {
+      const colorSet: ColorSet = new ColorSet()
+      colorSet.list = colorList.map(currentColor => color(currentColor))
+      pieSeries.colors = colorSet
+    }
+
+    const createLegend = (): void => {
+      chart.legend = new Legend()
+      chart.legend.useDefaultMarker = false
+      chart.legend.valueLabels.template.text = '{value.value}'
+      chart.legend.labels.template.fontSize = '14px'
+      chart.legend.valueLabels.template.fontSize = '14px'
+      const markerTemplate = chart.legend.markers.template
+      markerTemplate.width = 14
+      markerTemplate.height = 14
+      const marker = markerTemplate.children.getIndex(0)
+      marker.cornerRadius(12, 12, 12, 12)
+
+      chart.legend.labels.template.maxWidth = 150
+      chart.legend.labels.template.truncate = true
+    }
+
+    const initPieChart = (): void => {
+      chart = create(`chart-${name.value}`, PieChart)
+      chart.responsive.enabled = true
+      chart.data = data.value
+
+      createSeries()
+      createSeriesAnimations()
+      setColors()
+      createLegend()
+    }
+
+    watch(data, async (): Promise<void> => {
+      await nextTick()
+      if (chart) {
+        chart.dispose()
+      }
+      initPieChart()
+    })
+
+    onBeforeUnmount((): void => {
+      if (chart) {
+        chart.dispose()
+      }
+    })
 
     // expose to template
     return {
-      ceva
+      chartName: name.value
     }
   }
 })
 
-export default PieChart
+export default PieChartComponent
