21cdae2b63e55b488b053c72d451e7e40406f989
Florian.B <floryn93@gmail.com>
2020-12-04 00:10:31 +0200

    [photos] Remove vuex.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,2 @@
-import { createStore } from 'vuex'
 
-export default createStore({
-  state: {
-    token: localStorage.getItem(btoa('token')) || '',
-    isLoggedIn: !!localStorage.getItem(btoa('token'))
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
   }
-})
