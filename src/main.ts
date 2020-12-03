9de65cb2b6136d02b7b3be6a5c46901806905838
Florian.B <floryn93@gmail.com>
2020-12-04 00:17:17 +0200

    [photos] Add centralized state management.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+
 import './registerServiceWorker'
 import router from './router'
-import store from './store'
+import { createStore, storeSymbol } from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+app.config.globalProperties.window = window
+
+app.provide(storeSymbol, createStore())
+app.use(router).mount('#app')
