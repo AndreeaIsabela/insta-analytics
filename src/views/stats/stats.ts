50ae47b5781bf1094c1f50bcdde516ab839eafaa
AndreeaIsabela <bala.andreea@yahoo.com>
2020-12-16 20:18:25 +0200

    [main] Add video & album types to timeline chart.

--- src/views/stats/stats.ts
+++ src/views/stats/stats.ts
@@ -1,8 +1,9 @@
-import { defineComponent, reactive, onMounted } from 'vue'
+import { defineComponent, reactive } from 'vue'
 import moment from 'moment'
 
 import { useStore } from '@/store'
 import TimelineChart from '../../components/timelineChart/TimelineChart.vue'
+import { Photo } from '@/types/photo'
 
 const Stats = defineComponent({
   components: {
@@ -11,21 +12,31 @@ const Stats = defineComponent({
   setup () {
     const data: any = reactive([])
     const store: any = useStore()
+    const media = store.state.media
     const getDate = (timestamp: string) => {
       return moment(timestamp).format('L')
     }
-    onMounted((): void => {
-      for (const photo of store.state.media) {
-        const date = getDate(photo.timestamp)
+    const addMediaToData = (mediaList: Photo[], mediaType: string, mediaType2: string, mediaType3: string) => {
+      for (const media of mediaList) {
+        const date = getDate(media.timestamp)
         const dateIndex = data.findIndex((el: { date: string; photos: number }) => el.date === date)
+
         if (dateIndex > -1) {
-          data[dateIndex].photos += 1
+          data[dateIndex][mediaType] += 1
         } else {
-          const photos = 1
-          data.push({ date, photos })
+          const obj = {
+            date,
+            [mediaType]: 1,
+            [mediaType2]: 0,
+            [mediaType3]: 0
+          }
+          data.push(obj)
         }
       }
-    })
+    }
+    addMediaToData(media.photos, 'photos', 'video', 'album')
+    addMediaToData(media.video, 'video', 'photos', 'album')
+    addMediaToData(media.album, 'album', 'photos', 'video')
     // expose to template
     return {
       data
