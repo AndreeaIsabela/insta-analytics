ccb258ebeabc50005faa71c66dd0e05acdf39f93
AndreeaIsabela <bala.andreea@yahoo.com>
2021-01-21 00:30:31 +0200

    [main] Add media details get request.

--- src/api/instagram.ts
+++ src/api/instagram.ts
@@ -27,15 +27,15 @@ export class InstagramApi {
     return data
   }
   /**
-   * Get Instagram insights for the given user.
+   * Get Instagram media details for the given user.
    *
    * @param {string} token
    * @returns {Promise<AxiosResponse>}
    */
 
-  async getUserInsights (token: string): Promise<AxiosResponse> {
-    const metrics = 'audience_city,audience_gender_age,audience_gender_age'
-    const url = `https://graph.facebook.com/v9.0/me/insights?metric=${metrics}&period=lifetime&access_token=${token}`
+  async getUserMediaDetails (token: string, mediaID: string): Promise<AxiosResponse> {
+    const fields = 'id,media_type,media_url,timestamp,caption,username'
+    const url = `https://graph.instagram.com/${mediaID}?fields=${fields}&period=lifetime&access_token=${token}`
 
     const { data } = await this.http.get(url)
 
--- src/views/mediaDetails/mediaDetails.ts
+++ src/views/mediaDetails/mediaDetails.ts
@@ -1,13 +1,29 @@
 import { defineComponent } from 'vue'
 import { useRoute } from 'vue-router'
+import moment from 'moment'
+
+import { api } from '@/api'
 
 const MediaDetails = defineComponent({
   setup () {
     const route = useRoute()
-    const mediaId = route.params.id
+    const mediaId: string = route.params.id as string
+    const token: string | null = localStorage.getItem('accessToken')
+    let mediaDetails: any = {}
+
+    const getTime = (date: string) => moment(date).fromNow()
+    const checkToken = async () => {
+      if (token) {
+        const { data } = await api.instagram.getUserMediaDetails(token, mediaId)
+        mediaDetails = data
+      }
+    }
+    checkToken()
 
     return {
-      mediaId
+      mediaId,
+      mediaDetails,
+      getTime
     }
   }
 
